5ab9ee70fd1e203df3137dd450caf3d77ad5431a
Steve Csvihinka <stevecsvihinka@gmail.com>
2015-08-04 13:16:13 -0400

    heard a song all the way through without crashing

--- client/ccc.js
+++ client/ccc.js
@@ -4,14 +4,19 @@ if (Meteor.isClient) {
 
   Template.hello.events({
   'click #inputAudio':function(){
+    try{
      var file =  $('#inputAudio').get(0).files[0] //Some jQuery to get the value.
      fsFile = new FS.File(file);
      fsFile.metadata = {coolText:"coolText"} //FS.File support metadata.
      AudioCollection.insert(fsFile,function(err,result){
-      if(!err){
+      if(err){
         console.log(result) //you should get an id here since the full object take more less 10 sec to upload
         }
   })//insert
+   }
+   catch (err){
+    console.log(err);
+   }
   }
 })//events
 
