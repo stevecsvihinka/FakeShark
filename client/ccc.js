b1b73d181615b880de08f900a597634e1da54cfd
Steve Csvihinka <stevecsvihinka@gmail.com>
2015-08-06 14:40:36 -0400

    adds download link

--- client/ccc.js
+++ client/ccc.js
@@ -1,24 +1,29 @@
+  Template.hello.onCreated(function() {
   Meteor.subscribe('lecture_audio');
+  this.subscribe('lecture_audio');
+});
 
   Template.hello.events({
   'click #submitAudio':function(){
-     var file =  $('#inputAudio').get(0).files[0] //Some jQuery to get the value.
+     file =  $('#inputAudio').get(0).files[0] //Some jQuery to get the value.
      console.log(file);
      fsFile = new FS.File(file);
-     console.log(fsFile);
-
-     ID3.loadTags(fsFile, function() {
-     var tags = ID3.getAllTags(fsFile);
-     console.log(tags.artist + " - " + tags.title + ", " + tags.album);
-     });
+     console.log(fsFile);   
+  
+      ID3.loadTags(file.url, function() {
+      var tags = ID3.getAllTags(file.url);
+      console.log(tags.artist + " - " + tags.title + ", " + tags.album);
+      });
 
      fsFile.metadata = {coolText:"coolText"} //FS.File support metadata.
      AudioCollection.insert(fsFile);//insert
-
-setTimeout(function() { location.reload(); }, 5000);
-     
   }//function
 })//events
+
+  Template.outer.onCreated(function(){
+  Meteor.subscribe('lecture_audio');
+  this.subscribe('lecture_audio');
+});
 
   Template.hello.helpers({
   showAudio:function(){
@@ -26,14 +31,9 @@ setTimeout(function() { location.reload(); }, 5000);
   }
 })
 
-
 // Template.hello.rendered = function() {
-//      var file =  $('#inputAudio').get(0).files[0] //Some jQuery to get the value.
-//      fsFile = new FS.File(file);
-//      fsFile.metadata = {coolText:"coolText"} //FS.File support metadata.
-//      AudioCollection.insert(fsFile,function(err,result){
-//       if(err){
-//         console.log(result) //you should get an id here since the full object take more less 10 sec to upload
-//         }
-//   })//insert
-//   }//function
+//   ID3.loadTags(fsFile, function() {
+//      var tags = ID3.getAllTags(fsFile);
+//      console.log(tags.artist + " - " + tags.title + ", " + tags.album);
+//      });  
+//  }
