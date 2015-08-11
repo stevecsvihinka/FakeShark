8b7d6fa22741140839407c3161382f68f9fc6b5e
Steve Csvihinka <stevecsvihinka@gmail.com>
2015-08-10 22:39:20 -0400

    improves playlist functionality

--- client/ccc.js
+++ client/ccc.js
@@ -1,62 +1,55 @@
-  var _player = document.getElementById("player"),
-    _playlist = document.getElementById("playlist"),
-    _stop = document.getElementById("stop");
+var _player = document.getElementById("player"),
+_playlist = document.getElementById("playlist"),
+_stop = document.getElementById("stop");
 
-  Template.hello.onCreated(function() {
+Template.hello.onCreated(function() {
   Meteor.subscribe('lecture_audio');
   this.subscribe('lecture_audio');
 });
 
-  Template.hello.events({
+Template.hello.events({
   'click #submitAudio': 
-  submitAudio = function(){
-    
-     file =  $('#inputAudio').get(0).files[0] //Some jQuery to get the value.
-     console.log(file);
-     fsFile = new FS.File(file);
-     fsFileClone = fsFile;
-     console.log(fsFile);      
+  submitAudio = function(){  
+    file =  $('#inputAudio').get(0).files[0] //Some jQuery to get the value.
+    console.log(file);
+    fsFile = new FS.File(file);
+    fsFileClone = fsFile;
+    console.log(fsFile);      
 
-     ID3.loadTags('cfs/files/AudioCollection/'+fsFile._id+'/'+file.url, function() {
-     tags = ID3.getAllTags('cfs/files/AudioCollection/'+fsFile._id+'/'+file.url);
-     fsFile.metadata = {coolText: tags.artist + " - " + tags.title + ", " + tags.album}
-     });
+    ID3.loadTags('cfs/files/AudioCollection/'+fsFile._id+'/'+file.url, function() {
+    tags = ID3.getAllTags('cfs/files/AudioCollection/'+fsFile._id+'/'+file.url);
+    fsFile.metadata = {coolText: tags.artist + " - " + tags.title + ", " + tags.album}
+    });
 
-     AudioCollection.insert(fsFile);//insert
-
-     setTimeout(function(){  
-     id3tags();
-     }, 10000);
-
-     
+    AudioCollection.insert(fsFile);//insert
   }//function
-  })//events
+})//events
 
-  Template.hello.events({
+Template.hello.events({
   'click #songInfo': function() {
-      id3tags();
-    }
-  })
-
-  id3tags = function() {
-    ID3.loadTags('cfs/files/AudioCollection/'+fsFileClone._id+'/'+file.url, function() {
-      tags = ID3.getAllTags('cfs/files/AudioCollection/'+fsFileClone._id+'/'+file.url);
-      AudioCollection.update({_id: fsFileClone._id}, {$set: {coolText: tags.artist + " - " + tags.title + ", " + tags.album} });
-      });
-  }
-
-  Template.outer.onCreated(function(){
-  Meteor.subscribe('lecture_audio');
-  this.subscribe('lecture_audio');
-});
-
-  Template.hello.helpers({
-  showAudio:function(){
-   return AudioCollection.find();
+    id3tags();
   }
 })
 
-  Template.hello.events({
+id3tags = function() {
+  ID3.loadTags('cfs/files/AudioCollection/'+fsFileClone._id+'/'+file.url, function() {
+  tags = ID3.getAllTags('cfs/files/AudioCollection/'+fsFileClone._id+'/'+file.url);
+  AudioCollection.update({_id: fsFileClone._id}, {$set: {coolText: tags.artist + " - " + tags.title + ", " + tags.album} });
+  });
+}
+
+Template.outer.onCreated(function(){
+  Meteor.subscribe('lecture_audio');
+  this.subscribe('lecture_audio');
+});
+
+Template.hello.helpers({
+  showAudio:function(){
+    return AudioCollection.find();
+  }
+})
+
+Template.hello.events({
   'click .noblue': function(evt){
     tar = (evt.target); // target is clicked
     console.log(tar);
@@ -72,22 +65,16 @@
   })
 
 function playlistItemClick(clickedElement) {
-    clickedElement.classList.add("selected");
-    x = clickedElement;
-    var toBeRemoved = $( '.selected' ).first()[0];
-    if (toBeRemoved !== x)
-    {
-      toBeRemoved.classList.remove("selected");
-    }
-    abc = clickedElement.getAttribute("id");
-    $("#player").attr("src", abc);
-}
-
-function playNext() {
-    var selected = document.querySelector("li.a.selected");
-    console.log(selected);
-    console.log(selected.nextSibling);
-    if (selected && selected.nextSibling) {
-        playlistItemClick(selected.nextSibling);
-    }
+  clickedElement.classList.add("selected");
+  x = clickedElement;
+  var toBeRemoved = $( '.selected' );
+  for (i=0; i < toBeRemoved.length; i++)
+  {
+    if (toBeRemoved[i] !== x)
+      {
+        toBeRemoved[i].classList.remove("selected");
+      }
+  }    
+  abc = clickedElement.getAttribute("id");
+  $("#player").attr("src", abc);
 }
